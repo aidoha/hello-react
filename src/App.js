f01662d2735bd7d48377ae66136edbadd90b9ff2
Aidyn Ibrayev <personal@NB11MDL004>
2022-06-13 11:25:53 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Dalida from "./dalida";
+import cat from "./img/cat.jpeg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [counter, setCounter] = useState(0);
+	const increaseCounter = () => {
+		setCounter(counter + 1);
+	};
+
+	const decreaseCounter = () => {
+		setCounter(counter - 1);
+	};
+
+	const resetCounter = () => {
+		setCounter(0);
+	};
+	const helloWorld = <h1>Hello world</h1>;
+	const helloWorldString = "Hello world";
+
+	const math = 2 + 2;
+
+	const user = {
+		firstName: "Joe",
+		lastName: "Biden",
+	};
+
+	const getUsername = ({ firstName, lastName }) => {
+		return `Current president of US is ${firstName} ${lastName}`;
+	};
+
+	const age = 16;
+
+	const condRendering =
+		age >= 18 ? <div>You're an adult</div> : <div>Come later sweetie</div>;
+
+	const divStyle = {
+		backgroundColor: "green",
+		color: "white",
+	};
+
+	return (
+		<div>
+			{helloWorld}
+			<h1
+				style={{
+					color: "red",
+					fontSize: "30px",
+				}}
+			>
+				{helloWorldString}
+			</h1>
+			<div style={divStyle}>{math}</div>
+			<div>{getUsername(user)}</div>
+			<img src={cat} />
+			{condRendering}
+
+			<div className="hello-world"></div>
+			<Dalida />
+			<div className="counter-wrapper">
+				<div className="counter">{counter}</div>
+				<div className="actions">
+					<button className="increase" onClick={increaseCounter}>
+						Increase
+					</button>
+					<button className="decrease" onClick={decreaseCounter}>
+						Decrease
+					</button>
+				</div>
+				<div className="reset">
+					<button onClick={resetCounter}>reset</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/dalida.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Dalida = () => {
+	const hey = () => <div>hey</div>;
+	return (
+		<>
+			<div>hello</div>
+			<div>world</div>
+		</>
+	);
+};
+
+export default Dalida;
